10e04f2875a321d0eaa9b2d1e42a70e66baed66b
zanuarprasetyo <122775305+zanuarprasetyo@users.noreply.github.com>
2023-08-19 12:37:58 +0700

    Add files via upload

---
+++ pertemuan3/latihan1.php
@@ -0,0 +1,26 @@
+<?php
+// Pengulangan
+// for
+// while
+// do.. while
+// foreach : pengulangan khusus array
+
+// pake for
+// for( $i = 0; $i < 5; $i++) {
+//     echo "Hello World! <br>";
+// }
+
+// pake while
+// $i = 0;
+// while( $i < 5 ) {
+//     echo "Hello World! <br>";
+//     $i++;
+// }
+
+// pake do.. while
+$i = 0;
+do {
+    echo "Hello World! <br>";
+    $i++;
+} while( $i < 5 );
+?>
---
+++ pertemuan3/latihan2.php
@@ -0,0 +1,17 @@
+<?php
+// Pengkondisian / Percabangan
+// if else
+// if else if else
+// ternary
+// switch
+// ternary dan switch tdk dijelaskan di vid ini
+
+$x = 20;
+if( $x < 20 ) {
+    echo "Benar";
+} else if( $x == 20 ) {
+    echo "Bingo!";
+} else {
+    echo "Salah";
+}
+?>
